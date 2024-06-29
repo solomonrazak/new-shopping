69e5832c1bbd0b314be432105a8f3f3576422ffc
solomonrazak <solomonrazak99@gmail.com>
2024-06-29 14:35:51 -0700

    http client provider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { Products } from './../../types';
+import { ProductsService } from './../services/products.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor (
+    private ProductsService: ProductsService
+  ){}
+
+  ngOnInit(){
+    this.ProductsService.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5}).subscribe((products) => {
+      console.log(products.items)
+    })
+  }
 
 }

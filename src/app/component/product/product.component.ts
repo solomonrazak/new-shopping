c7257568e75f4640dcef74de33d24db0d211f76e
solomonrazak <solomonrazak99@gmail.com>
2024-07-01 04:31:40 -0700

    got stucked with binding issues

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,14 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { Product } from '../../../types';
+import { CommonModule } from '@angular/common';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RatingModule, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-  @input() product!: Product;
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  ngOnInit(){
+    this.productOutput.emit(this.product);
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Product, Products } from './../../types';
 import { ProductsService } from './../services/products.service';
 import { Component } from '@angular/core';
 import { ProductComponent } from '../component/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -14,7 +15,10 @@ export class HomeComponent {
   constructor(private ProductsService: ProductsService) {}
 
   products: Product[] = [];
+  onProductOutput(product: Product){
+  console.log(product, 'Output');
 
+  }
   ngOnInit() {
     this.ProductsService.getProducts('http://localhost:3000/clothes', {
       page: 0,

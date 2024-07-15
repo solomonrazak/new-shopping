cbb7e3ba578f38b0e25b070a5f1e29f4a649e999
solomonrazak <solomonrazak99@gmail.com>
2024-07-14 18:48:52 -0700

    edit pop up update

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule],
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss'
 })
 export class EditPopupComponent {
   @Input() display: boolean = false;
-  @Output() confirm = new EventEmitter<Product>();
+  
   @Output() cancel = new EventEmitter<void>();
+  @Input() header!: string; // this property will always be provided. // this will always have a value
 
   @Input() product: Product = {
     name: '',
@@ -20,6 +24,8 @@ export class EditPopupComponent {
     image: '',
     rating: 0,
   }
+
+  @Output() confirm = new EventEmitter<Product>();
 
   onConfirm() {
     this.confirm.emit(this.product);
